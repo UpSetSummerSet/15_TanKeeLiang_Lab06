f13fef688164c92ea7eb232e16060143e0dd916f
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2020-11-05 13:21:06 +0800

    Done

---
+++ 15_TanKeeLiang_Lab07/Assets/Scripts/InputControlScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControlScript : MonoBehaviour
+{
+    public GameObject upCube;
+    public GameObject downCube;
+    public GameObject leftCube;
+    public GameObject rightCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            upCube.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            upCube.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            downCube.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            downCube.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            leftCube.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            leftCube.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            rightCube.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            rightCube.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
+        }
+    }
+}
